c425c3c65c573fa945de71994af6ab82faa07274
Greg MacWilliam <greg.macwilliam@voxmedia.com>
2015-03-21 14:24:03 -0400

    Faster emoticon matching.

--- lib/gemoji-parser.rb
+++ lib/gemoji-parser.rb
@@ -4,13 +4,38 @@ require 'gemoji'
 module EmojiParser
   extend self
 
-  # Defines the parser's table of emoticons. May be customized!
-  # Table is formatted as... { ":-)": :smile }
-  attr_writer :emoticons
-
+  # Emoticons
+  # ---------
+  # The base emoticons set (below) is generated with "noseless" variants, ie:  :-) and :)
+  # The generated `EmojiParser.emoticons` hash is formatted as:
+  # ---
+  # > {
+  # >   ":-)" => :blush,
+  # >   ":)" => :blush,
+  # >   ":-D" => :smile,
+  # >   ":D" => :smile,
+  # > }
+  # 
+  # This base set is selected for commonality and high degrees of author intention.
+  # If you want more/different emoticons:
+  # - Please DO customize the `EmojiParser.emoticons` hash in your app runtime.
+  # - Please DO NOT customize this source code and issue a pull request.
+  #
+  # To add an emoticon:
+  # ---
+  # > EmojiParser.emoticons[':-$'] = :grimacing
+  # > EmojiParser.rehash!
+  # 
+  # To remove an emoticon:
+  # ---
+  # > EmojiParser.emoticons.delete(':-$')
+  # > EmojiParser.rehash!
+  # 
+  # NOTE: call `rehash!` after making changes to Emoji/emoticon sets.
+  # Rehashing updates the parser's regex cache with the latest icons.
+  #
   def emoticons
     return @emoticons if defined? @emoticons
-
     @emoticons = {}
     emoticons = {
       angry: ">:-(",
@@ -30,8 +55,8 @@ module EmojiParser
     }
 
     # Parse all named patterns into a flat hash table,
-    # where pattern is the key and its name is the value.
-    # all patterns are duplicated with the "-" (nose) omitted.
+    # where pattern is the key and its token is the value.
+    # all patterns are duplicated with the "noseless" variants, ie:  :-) and :)
     emoticons.each_pair do |name, patterns|
       patterns = [patterns] unless patterns.is_a?(Array)
       patterns.each do |pattern| 
@@ -43,6 +68,16 @@ module EmojiParser
     @emoticons
   end
 
+  attr_writer :emoticons
+
+  # Rehashes all cached regular expressions.
+  # IMPORTANT: call this once after changing emoji characters or emoticon patterns.
+  def rehash!
+    unicode_regex(rehash: true)
+    token_regex(rehash: true)
+    emoticon_regex(rehash: true)
+  end
+
   # DEPRECATED, TODO: remove in v1.2.x
   # - Options: rehash:boolean
   def emoji_regex(opts={})
@@ -50,76 +85,92 @@ module EmojiParser
     unicode_regex(opts)
   end
 
-  # Rehashes all cached parser regular expressions.
-  # Call this after adding custom symbols...
-  def rehash!
-    unicode_regex(rehash: true)
-    token_regex(rehash: true)
-    emoticon_regex(rehash: true)
-  end
-
-  # Provides a regular expression for matching unicode symbols.
+  # Creates an optimized regular expression for matching unicode symbols.
   # - Options: rehash:boolean
   def unicode_regex(opts={})
     return @unicode_regex if defined?(@unicode_regex) && !opts[:rehash]
-    patterns = []
+    pattern = []
 
     Emoji.all.each do |emoji|
       u = emoji.unicode_aliases.map do |str|
         str.codepoints.map { |c| '\u{%s}' % c.to_s(16).rjust(4, '0') }.join('')
       end
-      # Old way: 10x slower!
-      # patterns.concat u.sort! { |a, b| b.length - a.length }
-      patterns << unicode_matcher(u) if u.any?
+      # Simple method: x10 slower!
+      # pattern.concat u.sort! { |a, b| b.length - a.length }
+      pattern << unicode_matcher(u) if u.any?
     end
 
-    @unicode_pattern = patterns.join('|')
+    @unicode_pattern = pattern.join('|')
     @unicode_regex = Regexp.new("(#{@unicode_pattern})")
   end
 
-  # Provides a regular expression for matching token symbols.
+  # Creates a regular expression for matching token symbols.
+  # - Options: rehash:boolean (currently unused)
   def token_regex(opts={})
     return @token_regex if defined?(@token_regex)
     @token_pattern = ':([\w+-]+):'
     @token_regex = Regexp.new(@token_pattern)
   end
 
-  # Provides a regular expression for matching emoticon symbols.
+  # Creates an optimized regular expression for matching emoticon symbols.
   # - Options: rehash:boolean
   def emoticon_regex(opts={})
     return @emoticon_regex if defined?(@emoticon_regex) && !opts[:rehash]
-    pattern = emoticons.keys.map { |key| Regexp.escape(key) }.join('|')
-    @emoticon_pattern = "(?<=^|\\s)(?:#{ pattern })(?=\\s|$)"
+    pattern = {}
+
+    emoticons.keys.each do |icon|
+      compact_icon = icon.gsub('-', '')
+
+      # Check to see if this icon has a compact version, ex:  :-)  versus  :)
+      # One expression will match as many nose/noseless variants as possible.
+      if compact_icon != icon && emoticons[compact_icon]
+        compact_regex = Regexp.escape(icon).gsub('-', '-?')
+
+        # Keep this expression if it hasn't been defined yet,
+        # or if it's longer than a previously defined pattern.
+        if !pattern[compact_icon] || pattern[compact_icon].length < compact_regex.length
+          pattern[compact_icon] = compact_regex
+        end
+      elsif !pattern[icon]
+        pattern[icon] = Regexp.escape(icon)
+      end
+    end
+
+    @emoticon_pattern = "(?<=^|\\s)(?:#{ pattern.values.join('|') })(?=\\s|$)"
     @emoticon_regex = Regexp.new("(#{@emoticon_pattern})")
   end
 
-  # Generates a regex for matching all symbol sets.
+  # Generates a macro regex for matching one or more symbol sets.
+  # Regex uses various formats, based on symbol sets. Yields match as $1 OR $2
+  # T/EU:        (token-$1)|(emoticon-unicode-$2)
+  # T/E or T/U:  (token-$1)|(emoticon/unicode-$2)
+  # EU:          (emoticon/unicode-$1)
   # - Options: unicode:boolean, tokens:boolean, emoticons:boolean
   def macro_regex(opts={})
     unicode_regex if opts[:unicode]
     token_regex if opts[:tokens]
     emoticon_regex if opts[:emoticons]
-    regex = []
+    pattern = []
 
     if opts[:emoticons] && opts[:unicode]
-      regex << "(?:#{ @emoticon_pattern })"
-      regex << @unicode_pattern
+      pattern << "(?:#{ @emoticon_pattern })"
+      pattern << @unicode_pattern
     else
-      regex << @emoticon_pattern if opts[:emoticons]
-      regex << @unicode_pattern if opts[:unicode]
+      pattern << @emoticon_pattern if opts[:emoticons]
+      pattern << @unicode_pattern if opts[:unicode]
     end
 
-    regex = regex.any? ? "(#{ regex.join('|') })" : ""
+    pattern = pattern.any? ? "(#{ pattern.join('|') })" : ""
 
     if opts[:tokens]
-      if regex.empty?
-        regex = @token_pattern
+      if pattern.empty?
+        pattern = @token_pattern
       else
-        regex = "(?:#{ @token_pattern })|#{ regex }"
+        pattern = "(?:#{ @token_pattern })|#{ pattern }"
       end
     end
 
-    Regexp.new(regex)
+    Regexp.new(pattern)
   end
 
   # Parses all unicode symbols within a string.
@@ -166,8 +217,8 @@ module EmojiParser
     if opts.none?
       opts = { unicode: true, tokens: true, emoticons: true }
     elsif opts.one?
-      return parse_unicode(text) { |e| yield e } if opts[:unicode]
-      return parse_tokens(text) { |e| yield e } if opts[:tokens]
+      return parse_unicode(text)   { |e| yield e } if opts[:unicode]
+      return parse_tokens(text)    { |e| yield e } if opts[:tokens]
       return parse_emoticons(text) { |e| yield e } if opts[:emoticons]
     end
     text.gsub(macro_regex(opts)) do |match|
@@ -188,15 +239,17 @@ module EmojiParser
     parse_tokens(text) { |emoji| emoji.raw }
   end
 
-  # Finds an Emoji::Character instance for an unknown symbol.
-  # - symbol: an <Emoji::Character>, emoticon, unicode, or token.
+  # Finds an Emoji::Character instance for an unknown symbol type.
+  # - symbol: an <Emoji::Character>, or a unicode/token/emoticon string.
   def find(symbol)
     return symbol if (symbol.is_a?(Emoji::Character))
     symbol = emoticons[symbol].to_s if emoticons.has_key?(symbol)
     Emoji.find_by_alias(symbol) || Emoji.find_by_unicode(symbol) || nil
   end
 
-  # Gets a symbol file reference; optionally with a custom path.
+  # Gets the file reference for a symbol; optionally with a custom path.
+  # - symbol: an <Emoji::Character>, or a unicode/token/emoticon string.
+  # - path: a file path to sub into symbol's filename.
   def filepath(symbol, path=nil)
     emoji = find(symbol)
     return nil unless emoji
